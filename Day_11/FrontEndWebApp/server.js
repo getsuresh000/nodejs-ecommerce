0c93a3a33651437b4409dfb20fa302f6fe402692
suresh <37210069+getsuresh000@users.noreply.github.com>
2022-10-09 16:09:47 +0530

    Delete Day_11 directory

--- Day_11/FrontEndWebApp/server.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const app=express();
-app.use(express.static('public'));
-app.get("/", (req, res)=>{
-    res.sendFile("index.html");
-})
-app.listen(7000);
-console.log("server Side Javascript Code");
-console.log("Website is hosted on web server. Listening on port 7000");
