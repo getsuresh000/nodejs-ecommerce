bd617bd077072f5afbd795a042886b11a2d1b6ec
suresh <37210069+getsuresh000@users.noreply.github.com>
2022-10-09 16:11:08 +0530

    Delete MVCAPP1 directory

--- MVCAPP1/router.js
+++
@@ -1,7 +0,0 @@
-const homeController=require('./controllers/homecontroller');
-
-module.exports=function(app){
-    app.get('/',homeController.home);
-    app.get('/aboutus',homeController.aboutus);
-    app.get('/contact',homeController.contactus);
-}
