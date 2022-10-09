61fd5ebab9643482c60fb77bfb16c4f8d0b3bfcb
suresh <37210069+getsuresh000@users.noreply.github.com>
2022-10-09 16:11:20 +0530

    Delete MVCAPP2 directory

--- MVCAPP2/controllers/homecontroller.js
+++
@@ -1,9 +0,0 @@
-const dal=require('../models/dal');
-
-//Controller logic
-exports.myliking=(req, res)=>{
-    let model=dal.getPersonDetails();
-    // send model  and  View to View Engine
-    res.render('../views/profile',model);
-    //res.render('../views/liking',person);
-}
--- MVCAPP2/models/dal.js
+++
@@ -1,9 +0,0 @@
-exports.getPersonDetails=()=>{
-    let person={
-        name:'Mangesh',
-        fullname:'Mangesh Shitaram Kesari',
-        email:'mangesh.kesari@transflower.in',
-        contactnumber:'9881735654'
-    };
-    return person;
-}
